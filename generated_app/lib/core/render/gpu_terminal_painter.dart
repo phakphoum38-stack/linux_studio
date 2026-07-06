88246f9b688516e75346b58c53dbdbf78bbc6e2e
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-06 19:41:15 +0700

    Update gpu_terminal_painter.dart

--- generated_app/lib/core/render/gpu_terminal_painter.dart
+++ generated_app/lib/core/render/gpu_terminal_painter.dart
@@ -1,41 +1,12 @@
 import 'package:flutter/material.dart';
-import '../engine/screen_buffer.dart';
 
 class GpuTerminalPainter extends CustomPainter {
-  final ScreenBuffer buffer;
-
-  GpuTerminalPainter(this.buffer);
-
   @override
   void paint(Canvas canvas, Size size) {
-    final textPainter = TextPainter(
-      textDirection: TextDirection.ltr,
-    );
-
-    final cellW = size.width / buffer.cols;
-    final cellH = size.height / buffer.rows;
-
-    for (int y = 0; y < buffer.rows; y++) {
-      for (int x = 0; x < buffer.cols; x++) {
-        final c = buffer.buffer[y][x];
-
-        textPainter.text = TextSpan(
-          text: c.char,
-          style: const TextStyle(
-            color: Colors.white,
-            fontFamily: 'monospace',
-          ),
-        );
-
-        textPainter.layout();
-        textPainter.paint(
-          canvas,
-          Offset(x * cellW, y * cellH),
-        );
-      }
-    }
+    final paint = Paint()..color = Colors.black;
+    canvas.drawRect(Offset.zero & size, paint);
   }
 
   @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
