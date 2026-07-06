578892b4a6a27e1cd7c9ed207adc8b90ec31d93c
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-06 19:49:49 +0700

    Update terminal_state_machine.dart

--- generated_app/lib/core/engine/terminal_state_machine.dart
+++ generated_app/lib/core/engine/terminal_state_machine.dart
@@ -1,54 +1,37 @@
 import 'screen_buffer.dart';
-import 'ansi_csi_parser.dart';
 
 class TerminalStateMachine {
   final ScreenBuffer buffer;
 
   TerminalStateMachine(this.buffer);
 
-  void execute(dynamic event) {
-    if (event is String) {
-      buffer.write(event);
-      return;
+  void feedChar(String char) {
+    switch (char) {
+      case '\n':
+        buffer.cursorDown();
+        buffer.carriageReturn();
+        break;
+
+      case '\r':
+        buffer.carriageReturn();
+        break;
+
+      case '\b':
+        buffer.cursorBack();
+        break;
+
+      case '\t':
+        buffer.tab();
+        break;
+
+      default:
+        buffer.putChar(char);
     }
+  }
 
-    if (event is CsiEvent) {
-      switch (event.type) {
-        case 'A': // up
-          buffer.cursor.row =
-              (buffer.cursor.row - 1).clamp(0, buffer.rows - 1);
-          break;
-
-        case 'B': // down
-          buffer.cursor.row =
-              (buffer.cursor.row + 1).clamp(0, buffer.rows - 1);
-          break;
-
-        case 'C': // right
-          buffer.cursor.col =
-              (buffer.cursor.col + 1).clamp(0, buffer.cols - 1);
-          break;
-
-        case 'D': // left
-          buffer.cursor.col =
-              (buffer.cursor.col - 1).clamp(0, buffer.cols - 1);
-          break;
-
-        case 'H': // home
-          buffer.cursor.set(0, 0);
-          break;
-
-        case 'J': // clear screen
-          buffer.clear();
-          break;
-
-        case 'K': // clear line
-          final row = buffer.cursor.row;
-          for (int c = 0; c < buffer.cols; c++) {
-            buffer.buffer[row][c].char = ' ';
-          }
-          break;
-      }
+  void feed(String data) {
+    for (final rune in data.runes) {
+      feedChar(String.fromCharCode(rune));
     }
   }
 }
