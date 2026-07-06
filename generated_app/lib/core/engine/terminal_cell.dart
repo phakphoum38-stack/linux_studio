0eece1478f52362f2c05d10c684def870041600d
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-06 20:35:58 +0700

    Refactor TerminalCell to simplify color properties
    Refactor TerminalCell class to use fg and bg properties instead of foreground and background. Removed unused reset method and color getters.

--- generated_app/lib/core/engine/terminal_cell.dart
+++ generated_app/lib/core/engine/terminal_cell.dart
@@ -1,11 +1,8 @@
-import 'package:flutter/material.dart';
-
-/// Represents one cell in the terminal screen.
 class TerminalCell {
   String char;
 
-  int foreground;
-  int background;
+  int fg;
+  int bg;
 
   bool bold;
   bool italic;
@@ -13,9 +10,9 @@ class TerminalCell {
   bool inverse;
 
   TerminalCell({
-    this.char = ' ',
-    this.foreground = 37,
-    this.background = 40,
+    required this.char,
+    this.fg = 37,
+    this.bg = 40,
     this.bold = false,
     this.italic = false,
     this.underline = false,
@@ -25,67 +22,12 @@ class TerminalCell {
   TerminalCell copy() {
     return TerminalCell(
       char: char,
-      foreground: foreground,
-      background: background,
+      fg: fg,
+      bg: bg,
       bold: bold,
       italic: italic,
       underline: underline,
       inverse: inverse,
     );
-  }
-
-  void reset() {
-    char = ' ';
-    foreground = 37;
-    background = 40;
-    bold = false;
-    italic = false;
-    underline = false;
-    inverse = false;
-  }
-
-  Color get foregroundColor {
-    switch (foreground) {
-      case 30:
-        return Colors.black;
-      case 31:
-        return Colors.red;
-      case 32:
-        return Colors.green;
-      case 33:
-        return Colors.yellow;
-      case 34:
-        return Colors.blue;
-      case 35:
-        return Colors.purple;
-      case 36:
-        return Colors.cyan;
-      case 37:
-      default:
-        return Colors.white;
-    }
-  }
-
-  Color get backgroundColor {
-    switch (background) {
-      case 40:
-        return Colors.black;
-      case 41:
-        return Colors.red;
-      case 42:
-        return Colors.green;
-      case 43:
-        return Colors.yellow;
-      case 44:
-        return Colors.blue;
-      case 45:
-        return Colors.purple;
-      case 46:
-        return Colors.cyan;
-      case 47:
-        return Colors.white;
-      default:
-        return Colors.black;
-    }
   }
 }
