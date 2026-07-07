c236726536341e1b6654912669bc27b2d4b9d0ac
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-07 09:39:54 +0700

    Update terminal_view.dart

--- generated_app/lib/core/engine/terminal_view.dart
+++ generated_app/lib/core/engine/terminal_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import '../core/engine/screen_buffer.dart';
 
 class TerminalView extends StatefulWidget {
@@ -18,68 +19,133 @@ class _TerminalViewState extends State<TerminalView> {
   void initState() {
     super.initState();
 
-    widget.buffer.cursor.startBlink(() {
-      setState(() {});
-    });
+    // Optional cursor blinking.
+    try {
+      widget.buffer.cursor.startBlink(() {
+        if (mounted) {
+          setState(() {});
+        }
+      });
+    } catch (_) {
+      // Cursor blinking not implemented.
+    }
   }
 
   @override
   void dispose() {
-    widget.buffer.cursor.stopBlink();
+    try {
+      widget.buffer.cursor.stopBlink();
+    } catch (_) {}
+
     super.dispose();
   }
 
-  Color _color(int code) {
+  Color _ansiColor(int code) {
     switch (code) {
-      case 1:
+      case 30:
+        return Colors.black;
+      case 31:
         return Colors.red;
-      case 2:
+      case 32:
         return Colors.green;
-      case 3:
+      case 33:
         return Colors.yellow;
-      case 4:
+      case 34:
         return Colors.blue;
+      case 35:
+        return Colors.purple;
+      case 36:
+        return Colors.cyan;
+      case 37:
       default:
         return Colors.white;
     }
   }
 
+  Color _ansiBackground(int code) {
+    switch (code) {
+      case 40:
+        return Colors.black;
+      case 41:
+        return Colors.red;
+      case 42:
+        return Colors.green;
+      case 43:
+        return Colors.yellow;
+      case 44:
+        return Colors.blue;
+      case 45:
+        return Colors.purple;
+      case 46:
+        return Colors.cyan;
+      case 47:
+        return Colors.white;
+      default:
+        return Colors.transparent;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final b = widget.buffer;
+    final screen = widget.buffer;
 
     return Container(
       color: Colors.black,
       padding: const EdgeInsets.all(8),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: List.generate(b.rows, (r) {
-          return Row(
-            children: List.generate(b.cols, (c) {
-              final cell = b.buffer[r][c];
+        children: List.generate(
+          screen.rows,
+          (row) {
+            return Row(
+              mainAxisSize: MainAxisSize.min,
+              children: List.generate(
+                screen.cols,
+                (col) {
+                  final cell = screen.buffer[row][col];
 
-              final isCursor =
-                  b.cursor.visible &&
-                  b.cursor.row == r &&
-                  b.cursor.col == c;
+                  bool cursorVisible = false;
 
-              return Container(
-                width: 10,
-                height: 18,
-                color: isCursor
-                    ? Colors.white
-                    : Colors.transparent,
-                child: Text(
-                  cell.char,
-                  style: TextStyle(
-                    color: _color(cell.fg),
-                    fontFamily: 'monospace',
-                  ),
-                ),
-              );
-            }),
-          );
-        }),
+                  try {
+                    cursorVisible =
+                        screen.cursor.visible &&
+                        screen.cursor.row == row &&
+                        screen.cursor.col == col;
+                  } catch (_) {}
+
+                  return Container(
+                    width: 10,
+                    height: 18,
+                    alignment: Alignment.center,
+                    color: cursorVisible
+                        ? Colors.white
+                        : _ansiBackground(cell.background),
+                    child: Text(
+                      cell.char,
+                      style: TextStyle(
+                        color: cursorVisible
+                            ? Colors.black
+                            : _ansiColor(cell.foreground),
+                        fontFamily: 'monospace',
+                        fontWeight: cell.bold
+                            ? FontWeight.bold
+                            : FontWeight.normal,
+                        fontStyle: cell.italic
+                            ? FontStyle.italic
+                            : FontStyle.normal,
+                        decoration: cell.underline
+                            ? TextDecoration.underline
+                            : TextDecoration.none,
+                        fontSize: 14,
+                        height: 1.0,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        ),
       ),
     );
   }
