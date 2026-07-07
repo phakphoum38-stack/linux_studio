d8c6b5974fc7f20394966fbd61b699db9b4c1494
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-07 11:14:29 +0700

    Update terminal_engine.dart

--- generated_app/lib/core/engine/terminal_engine.dart
+++ generated_app/lib/core/engine/terminal_engine.dart
@@ -1,45 +1,164 @@
-import 'dart:io';
+import 'dart:async';
 import 'dart:convert';
+import 'dart:io';
+
 import 'ansi_csi_parser.dart';
+import 'screen_buffer.dart';
+import 'vt100_controller.dart';
+
 
 class TerminalEngine {
+
   Process? _process;
 
-  final AnsiCsiParser parser = AnsiCsiParser();
 
-  Function(dynamic event)? onEvent;
+  final ScreenBuffer buffer;
 
-  Future<void> start(Function(dynamic event) callback) async {
-    onEvent = callback;
 
-    _process = await Process.start(
-      'bash',
-      [],
-      runInShell: true,
-    );
+  final AnsiCsiParser parser =
+      AnsiCsiParser();
 
-    _process!.stdout.transform(utf8.decoder).listen((data) {
-      final events = parser.parse(data);
 
-      for (final e in events) {
-        onEvent?.call(e);
-      }
-    });
+  final VT100Controller vt100 =
+      VT100Controller();
 
-    _process!.stderr.transform(utf8.decoder).listen((data) {
-      final events = parser.parse(data);
 
-      for (final e in events) {
-        onEvent?.call(e);
-      }
-    });
+
+  Function()? onUpdate;
+
+
+
+  TerminalEngine({
+    required this.buffer,
+  });
+
+
+
+  Future<void> start() async {
+
+
+    _process =
+        await Process.start(
+          'bash',
+          [
+            '-i'
+          ],
+          runInShell: true,
+        );
+
+
+
+    _process!
+        .stdout
+        .transform(
+          utf8.decoder,
+        )
+        .listen(
+          _handleOutput,
+        );
+
+
+
+    _process!
+        .stderr
+        .transform(
+          utf8.decoder,
+        )
+        .listen(
+          _handleOutput,
+        );
+
   }
 
-  void write(String input) {
-    _process?.stdin.writeln(input);
+
+
+
+
+
+  void _handleOutput(
+    String data,
+  ){
+
+    final events =
+        parser.parse(data);
+
+
+
+    for(final event in events){
+
+      if(event is String){
+
+        buffer.writeText(
+          event,
+        );
+
+
+      }
+
+      else {
+
+        vt100.execute(
+          event.command,
+          event.args,
+          buffer,
+        );
+
+      }
+
+    }
+
+
+    onUpdate?.call();
+
   }
 
-  void kill() {
+
+
+
+
+
+  void write(
+    String input,
+  ){
+
+    if(_process == null)
+      return;
+
+
+    _process!
+        .stdin
+        .write(
+          input,
+        );
+
+  }
+
+
+
+
+
+  void resize(
+    int cols,
+    int rows,
+  ){
+
+    // Phase 16.2
+    // PTY resize
+
+
+  }
+
+
+
+
+
+  Future<void> kill()
+  async {
+
     _process?.kill();
+
+    _process=null;
+
   }
+
 }
