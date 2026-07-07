0fbe786f5b2b2ebe06d0a638f48def5df3ee547c
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-07 18:55:29 +0700

    Refactor pty functions for improved readability

--- generated_app/native/pty/pty.c
+++ generated_app/native/pty/pty.c
@@ -7,124 +7,62 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 static int master_fd = -1;
+static pid_t child_pid = -1;
 
-static pid_t child_pid;
+int pty_read(char* buffer, int size) {
+    if (master_fd < 0) return -1;
+    return (int)read(master_fd, buffer, (size_t)size);
+}
 
-
-
-int pty_spawn(
-    const char* shell,
-    int cols,
-    int rows
-){
+int pty_spawn(const char* shell, int cols, int rows) {
+    if (!shell) shell = "/bin/bash";
 
     struct winsize ws;
+    memset(&ws, 0, sizeof(ws));
+    ws.ws_col = (unsigned short)cols;
+    ws.ws_row = (unsigned short)rows;
 
+    child_pid = forkpty(&master_fd, NULL, NULL, &ws);
 
-    ws.ws_col = cols;
-    ws.ws_row = rows;
-
-
-
-    child_pid =
-        forkpty(
-            &master_fd,
-            NULL,
-            NULL,
-            &ws
-        );
-
-
-
-    if(child_pid == 0){
-
-        setenv(
-            "TERM",
-            "xterm-256color",
-            1
-        );
-
-
-        execl(
-            shell,
-            shell,
-            "-i",
-            NULL
-        );
-
-
-        exit(1);
-
+    if (child_pid < 0) {
+        master_fd = -1;
+        return -1;
     }
 
+    if (child_pid == 0) {
+        setenv("TERM", "xterm-256color", 1);
+        execl(shell, shell, "-i", (char*)NULL);
+        _exit(127);
+    }
 
     return master_fd;
-
 }
 
-
-
-
-
-
-
-int pty_write(
-    const char* data,
-    int length
-){
-
-    return write(
-        master_fd,
-        data,
-        length
-    );
-
+int pty_write(const char* data, int length) {
+    if (master_fd < 0) return -1;
+    return (int)write(master_fd, data, (size_t)length);
 }
 
-
-
-
-
-
-
-int pty_resize(
-    int cols,
-    int rows
-){
+int pty_resize(int cols, int rows) {
+    if (master_fd < 0) return -1;
 
     struct winsize ws;
+    memset(&ws, 0, sizeof(ws));
+    ws.ws_col = (unsigned short)cols;
+    ws.ws_row = (unsigned short)rows;
 
-
-    ws.ws_col = cols;
-    ws.ws_row = rows;
-
-
-
-    return ioctl(
-        master_fd,
-        TIOCSWINSZ,
-        &ws
-    );
-
+    return ioctl(master_fd, TIOCSWINSZ, &ws);
 }
 
-
-
-
-
-
-
-void pty_kill(){
-
-    if(child_pid){
-
-        kill(
-            child_pid,
-            SIGKILL
-        );
-
+void pty_kill(void) {
+    if (child_pid > 0) {
+        kill(child_pid, SIGTERM);
+        child_pid = -1;
     }
 
+    if (master_fd >= 0) {
+        close(master_fd);
+        master_fd = -1;
+    }
 }
