44b80c9863c848a80a491185243bf9ccfb05e563
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-07 10:21:02 +0700

    Update terminal_view.dart

--- generated_app/lib/ui/terminal_view.dart
+++ generated_app/lib/ui/terminal_view.dart
@@ -1,35 +1,35 @@
 import 'package:flutter/material.dart';
+
 import '../core/render/terminal_painter.dart';
 import '../core/engine/screen_buffer.dart';
 
 class TerminalView extends StatefulWidget {
   final ScreenBuffer buffer;
-  final int cursorRow;
-  final int cursorCol;
 
   const TerminalView({
     super.key,
     required this.buffer,
-    required this.cursorRow,
-    required this.cursorCol,
   });
 
   @override
-  State<TerminalView> createState() => _TerminalViewState();
+  State<TerminalView> createState() =>
+      _TerminalViewState();
 }
 
-class _TerminalViewState extends State<TerminalView>
-    with SingleTickerProviderStateMixin {
+class _TerminalViewState extends State<TerminalView> {
 
   @override
-  Widget build(BuildContext context) {
-    return CustomPaint(
-      painter: TerminalPainter(
-        buffer: widget.buffer,
-        cursorRow: widget.cursorRow,
-        cursorCol: widget.cursorCol,
+  Widget build(
+    BuildContext context,
+  ) {
+    return Container(
+      color: Colors.black,
+      child: CustomPaint(
+        painter: TerminalPainter(
+          widget.buffer,
+        ),
+        size: Size.infinite,
       ),
-      child: Container(),
     );
   }
 }
