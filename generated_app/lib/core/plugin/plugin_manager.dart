1caee0963bb46de4270782520663aec98a367ce7
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-07 10:11:57 +0700

    Update plugin_manager.dart

--- generated_app/lib/core/plugin/plugin_manager.dart
+++ generated_app/lib/core/plugin/plugin_manager.dart
@@ -1,24 +1,46 @@
+import 'plugin.dart';
+
 class PluginManager {
-  final List<TerminalPlugin> plugins = [];
+  final List<TerminalPlugin> _plugins = [];
+
+  List<TerminalPlugin> get plugins =>
+      List.unmodifiable(_plugins);
 
   void register(TerminalPlugin plugin) {
-    plugins.add(plugin);
+    if (_plugins.contains(plugin)) {
+      return;
+    }
+
+    _plugins.add(plugin);
     plugin.onInit();
   }
 
-  void emitData(String data) {
-    for (final p in plugins) {
-      p.onData(data);
+  void unregister(TerminalPlugin plugin) {
+    if (_plugins.remove(plugin)) {
+      plugin.onDispose();
     }
   }
 
-  void emitCommand(String cmd, List<int> args) {
-    for (final p in plugins) {
-      p.onCommand(cmd, args);
+  void emitData(String data) {
+    for (final plugin in _plugins) {
+      plugin.onData(data);
+    }
+  }
+
+  void emitCommand(
+    String command,
+    List<int> args,
+  ) {
+    for (final plugin in _plugins) {
+      plugin.onCommand(command, args);
     }
   }
 
   void dispose() {
-    plugins.clear();
+    for (final plugin in _plugins) {
+      plugin.onDispose();
+    }
+
+    _plugins.clear();
   }
 }
