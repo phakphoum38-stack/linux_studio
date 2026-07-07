3b14a13fd353a738db900c3eedf75e208ea9fa38
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-07 14:07:38 +0700

    Implement NativePtyBackend class for terminal backend

---
+++ generated_app/lib/core/backend/native_pty_backend.dart
@@ -0,0 +1,64 @@
+class NativePtyBackend
+    implements TerminalBackend {
+
+
+@override
+Function(String)? onOutput;
+
+
+@override
+Function(String)? onError;
+
+
+
+final NativePty pty =
+    NativePty();
+
+
+
+@override
+Future<void> start()
+async {
+
+
+ // forkpty()
+
+}
+
+
+
+@override
+void write(
+ String data
+){
+
+ // pty_write()
+
+}
+
+
+
+@override
+void resize(
+ int cols,
+ int rows
+){
+
+ // ioctl()
+
+}
+
+
+
+@override
+Future<void> stop()
+async {
+
+
+ // kill child
+
+
+}
+
+
+}
