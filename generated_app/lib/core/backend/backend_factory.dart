39131411fd01763d02ad5a03edf90b1c14383bc5
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-07 12:52:24 +0700

    Refactor BackendFactory to support TerminalMode

--- generated_app/lib/core/backend/backend_factory.dart
+++ generated_app/lib/core/backend/backend_factory.dart
@@ -1,57 +1,119 @@
 import 'dart:io';
 
-
 import 'terminal_backend.dart';
 
 import 'pty_terminal_backend.dart';
-
 import 'ssh_terminal_backend.dart';
+
+
+
+enum TerminalMode {
+
+  local,
+
+  ssh,
+
+}
+
+
 
 
 
 class BackendFactory {
 
 
- static TerminalBackend create(){
 
+  static TerminalBackend create({
 
-   if(
-     Platform.isLinux ||
-     Platform.isMacOS
-   ){
+    TerminalMode mode =
+        TerminalMode.local,
 
-     return PtyTerminalBackend();
-
-   }
+  }) {
 
 
 
-   if(
-     Platform.isAndroid ||
-     Platform.isIOS
-   ){
-
-     return SshTerminalBackend();
-
-   }
+    switch(mode){
 
 
 
-   if(
-     Platform.isWindows
-   ){
+      case TerminalMode.local:
 
-     return PtyTerminalBackend();
-
-   }
+        return _createLocal();
 
 
 
-   throw UnsupportedError(
-     "Platform not supported",
-   );
+      case TerminalMode.ssh:
 
- }
+        return SshTerminalBackend();
+
+
+    }
+
+  }
+
+
+
+
+
+
+
+  static TerminalBackend _createLocal(){
+
+
+
+    if(
+      Platform.isLinux ||
+      Platform.isMacOS
+    ){
+
+      return PtyTerminalBackend();
+
+    }
+
+
+
+
+
+    if(
+      Platform.isWindows
+    ){
+
+      // Phase 16.9
+      // replace with ConPTY backend
+
+      return PtyTerminalBackend();
+
+    }
+
+
+
+
+
+    throw UnsupportedError(
+
+      "Local terminal is not supported on this platform"
+
+    );
+
+
+  }
+
+
+
+
+
+
+
+  static TerminalBackend createSSH(){
+
+
+    return SshTerminalBackend();
+
+
+  }
+
+
+
 
 
 }
