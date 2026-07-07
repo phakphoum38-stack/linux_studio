325f44f5cc4580924a11b8b479bf90aee690b21f
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-07 13:08:19 +0700

    Update terminal_keyboard.dart

--- generated_app/lib/core/input/terminal_keyboard.dart
+++ generated_app/lib/core/input/terminal_keyboard.dart
@@ -1,84 +1,54 @@
 class TerminalKeyboard {
 
 
-  static String key(
-    String name,
-  ) {
+  static String handle(
+    String key,
+  ){
+
+    switch(key){
 
 
-    switch(name.toUpperCase()) {
-
-
-      // Cursor
-
-      case "UP":
+      case "ArrowUp":
         return "\x1b[A";
 
 
-      case "DOWN":
+      case "ArrowDown":
         return "\x1b[B";
 
 
-      case "RIGHT":
+      case "ArrowRight":
         return "\x1b[C";
 
 
-      case "LEFT":
+      case "ArrowLeft":
         return "\x1b[D";
 
 
 
-      // Navigation
-
-      case "HOME":
+      case "Home":
         return "\x1b[H";
 
 
-      case "END":
+      case "End":
         return "\x1b[F";
 
 
-      case "PAGE_UP":
-        return "\x1b[5~";
-
-
-      case "PAGE_DOWN":
-        return "\x1b[6~";
-
-
-
-      // Editing
-
-      case "BACKSPACE":
-        return "\x7f";
-
-
-      case "DELETE":
+      case "Delete":
         return "\x1b[3~";
 
 
-      case "INSERT":
-        return "\x1b[2~";
+
+      case "Backspace":
+        return "\x7f";
 
 
-
-      // Control
-
-      case "ENTER":
+      case "Enter":
         return "\r";
 
 
-      case "TAB":
+      case "Tab":
         return "\t";
 
-
-      case "ESC":
-        return "\x1b";
-
-
-
-      case "CTRL+A":
-        return "\x01";
 
 
       case "CTRL+C":
@@ -89,102 +59,16 @@ class TerminalKeyboard {
         return "\x04";
 
 
-      case "CTRL+E":
-        return "\x05";
-
-
-      case "CTRL+L":
-        return "\x0c";
-
-
       case "CTRL+Z":
         return "\x1a";
 
 
 
-      // Function keys
-
-      case "F1":
-        return "\x1bOP";
-
-
-      case "F2":
-        return "\x1bOQ";
-
-
-      case "F3":
-        return "\x1bOR";
-
-
-      case "F4":
-        return "\x1bOS";
-
-
-      case "F5":
-        return "\x1b[15~";
-
-
-      case "F6":
-        return "\x1b[17~";
-
-
-      case "F7":
-        return "\x1b[18~";
-
-
-      case "F8":
-        return "\x1b[19~";
-
-
-      case "F9":
-        return "\x1b[20~";
-
-
-      case "F10":
-        return "\x1b[21~";
-
-
-      case "F11":
-        return "\x1b[23~";
-
-
-      case "F12":
-        return "\x1b[24~";
-
-
-
       default:
 
-        return name;
+        return key;
 
     }
-
-  }
-
-
-
-
-
-  static String ctrl(
-    String char,
-  ) {
-
-    if(char.isEmpty){
-      return '';
-    }
-
-
-    final code =
-        char.toUpperCase().codeUnitAt(0)
-        - 64;
-
-
-    if(code < 1 || code > 26){
-      return char;
-    }
-
-
-    return String.fromCharCode(code);
 
   }
 
