d9be3499cb8cfa73e361a294461ce6780f15b6a2
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-07 10:54:12 +0700

    Update terminal_painter.dart

--- generated_app/lib/core/render/terminal_painter.dart
+++ generated_app/lib/core/render/terminal_painter.dart
@@ -2,72 +2,126 @@ import 'package:flutter/material.dart';
 
 import '../engine/screen_buffer.dart';
 
-class TerminalPainter extends CustomPainter {
+
+class TerminalPainter
+    extends CustomPainter {
+
+
   final ScreenBuffer screen;
 
+
   final double fontSize;
+
   final String fontFamily;
+
+
 
   TerminalPainter(
     this.screen, {
+
     this.fontSize = 14,
+
     this.fontFamily = 'monospace',
   });
+
+
 
   @override
   void paint(
     Canvas canvas,
     Size size,
   ) {
+
+
     canvas.drawRect(
       Offset.zero & size,
-      Paint()..color = Colors.black,
+
+      Paint()
+        ..color = Colors.black,
     );
 
-    final baseStyle = TextStyle(
+
+
+    final style = TextStyle(
+
       fontSize: fontSize,
+
       fontFamily: fontFamily,
+
       color: Colors.white,
     );
 
-    final rowHeight = fontSize * 1.25;
-    final colWidth = fontSize * 0.62;
 
-    for (int row = 0; row < screen.rows; row++) {
-      for (int col = 0; col < screen.cols; col++) {
-        final cell = screen.buffer[row][col];
 
-        if (cell.char == ' ') {
-          continue;
-        }
+    final rowHeight =
+        fontSize * 1.25;
 
-        final textPainter = TextPainter(
+
+    final colWidth =
+        fontSize * 0.62;
+
+
+
+    for (int r = 0;
+        r < screen.rows;
+        r++) {
+
+
+
+      for (int c = 0;
+          c < screen.cols;
+          c++) {
+
+
+
+        final cell =
+            screen.buffer[r][c];
+
+
+
+        final cursor =
+            screen.cursor.row == r &&
+            screen.cursor.col == c;
+
+
+
+        final painter =
+            TextPainter(
+
           text: TextSpan(
+
             text: cell.char,
-            style: baseStyle.copyWith(
-              color: _ansiColor(cell.fg),
-              backgroundColor: _ansiBg(cell.bg),
-              fontWeight: cell.bold
-                  ? FontWeight.bold
-                  : FontWeight.normal,
-              decoration: cell.underline
-                  ? TextDecoration.underline
-                  : TextDecoration.none,
-              fontStyle: cell.italic
-                  ? FontStyle.italic
-                  : FontStyle.normal,
+
+            style: style.copyWith(
+
+              color:
+                  _fg(cell.fg),
+
+              backgroundColor:
+                  cursor
+                      ? Colors.white
+                      : _bg(cell.bg),
             ),
           ),
-          textDirection: TextDirection.ltr,
+
+
+          textDirection:
+              TextDirection.ltr,
         );
 
-        textPainter.layout();
 
-        textPainter.paint(
+
+        painter.layout();
+
+
+
+        painter.paint(
+
           canvas,
+
           Offset(
-            col * colWidth,
-            row * rowHeight,
+            c * colWidth,
+            r * rowHeight,
           ),
         );
       }
@@ -75,61 +129,64 @@ class TerminalPainter extends CustomPainter {
   }
 
 
-  Color _ansiColor(
-    int value,
+
+
+  Color _fg(
+    int code,
   ) {
-    switch (value) {
-      case 30:
-        return Colors.black;
+
+    switch(code) {
+
       case 31:
         return Colors.red;
+
       case 32:
         return Colors.green;
+
       case 33:
         return Colors.yellow;
+
       case 34:
         return Colors.blue;
-      case 35:
-        return Colors.purple;
+
       case 36:
         return Colors.cyan;
-      case 37:
+
       default:
         return Colors.white;
     }
   }
 
 
-  Color _ansiBg(
-    int value,
+
+
+  Color _bg(
+    int code,
   ) {
-    switch (value) {
-      case 40:
-        return Colors.black;
+
+    switch(code) {
+
       case 41:
         return Colors.red;
+
       case 42:
         return Colors.green;
-      case 43:
-        return Colors.yellow;
+
       case 44:
         return Colors.blue;
-      case 45:
-        return Colors.purple;
-      case 46:
-        return Colors.cyan;
-      case 47:
-        return Colors.white;
+
       default:
         return Colors.transparent;
     }
   }
 
 
+
   @override
   bool shouldRepaint(
     covariant TerminalPainter oldDelegate,
   ) {
+
     return true;
   }
 }
