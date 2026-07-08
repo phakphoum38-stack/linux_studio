d6de9e85ade500807700e416dbda0d4924b477a2
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-08 18:40:26 +0700

    Update pipe.h

--- generated_app/native/windows/pipe.h
+++ generated_app/native/windows/pipe.h
@@ -1,4 +1,57 @@
-bool createPipes();
-bool read(char* buffer, int size);
-bool write(const char* data, int len);
-void close();
+#pragma once
+
+#ifdef _WIN32
+
+#include <windows.h>
+
+
+class PipeManager {
+
+private:
+
+    HANDLE inputRead;
+    HANDLE inputWrite;
+
+    HANDLE outputRead;
+    HANDLE outputWrite;
+
+
+public:
+
+    PipeManager();
+
+    ~PipeManager();
+
+
+    bool createPipes();
+
+
+    bool read(
+        char* buffer,
+        int size
+    );
+
+
+    bool write(
+        const char* data,
+        int len
+    );
+
+
+    void close();
+
+
+
+    HANDLE getInputRead() const;
+
+    HANDLE getInputWrite() const;
+
+
+    HANDLE getOutputRead() const;
+
+    HANDLE getOutputWrite() const;
+
+
+};
+
+#endif
