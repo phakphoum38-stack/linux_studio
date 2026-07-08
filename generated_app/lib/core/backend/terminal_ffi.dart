c4454633ffe2ad87ac4bbbfd41bf2e629e58ed88
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-08 21:53:37 +0700

    Update terminal_ffi.dart

--- generated_app/lib/core/backend/terminal_ffi.dart
+++ generated_app/lib/core/backend/terminal_ffi.dart
@@ -1,120 +1,118 @@
 import 'dart:ffi';
-import 'dart:io';
 
 import 'package:ffi/ffi.dart';
 
-typedef _CreateNative = Pointer<Void> Function(
-  Int32 rows,
-  Int32 cols,
-);
+import 'terminal_ffi.dart';
 
-typedef _Create = Pointer<Void> Function(
-  int rows,
-  int cols,
-);
+class NativeTerminal {
+  NativeTerminal({
+    this.rows = 24,
+    this.cols = 80,
+  });
 
-typedef _WriteNative = Uint8 Function(
-  Pointer<Void>,
-  Pointer<Utf8>,
-  Int32,
-);
+  final int rows;
 
-typedef _Write = int Function(
-  Pointer<Void>,
-  Pointer<Utf8>,
-  int,
-);
+  final int cols;
 
-typedef _ReadNative = Int32 Function(
-  Pointer<Void>,
-  Pointer<Utf8>,
-  Int32,
-);
+  Pointer<Void>? _handle;
 
-typedef _Read = int Function(
-  Pointer<Void>,
-  Pointer<Utf8>,
-  int,
-);
+  bool get isOpen =>
+      _handle != null &&
+      _handle != nullptr;
 
-typedef _ResizeNative = Uint8 Function(
-  Pointer<Void>,
-  Int32,
-  Int32,
-);
-
-typedef _Resize = int Function(
-  Pointer<Void>,
-  int,
-  int,
-);
-
-typedef _CloseNative = Void Function(
-  Pointer<Void>,
-);
-
-typedef _Close = void Function(
-  Pointer<Void>,
-);
-
-class TerminalFFI {
-  TerminalFFI._() {
-    _load();
-  }
-
-  static final TerminalFFI instance = TerminalFFI._();
-
-  late final DynamicLibrary library;
-
-  late final _Create create;
-
-  late final _Write write;
-
-  late final _Read read;
-
-  late final _Resize resize;
-
-  late final _Close close;
-
-  void _load() {
-    if (!Platform.isWindows) {
-      throw UnsupportedError(
-        'ConPTY backend is only supported on Windows.',
-      );
+  bool open() {
+    if (isOpen) {
+      return true;
     }
 
-    library = DynamicLibrary.open(
-      'terminal_api.dll',
+    _handle = TerminalFFI.instance.create(
+      rows,
+      cols,
     );
 
-    create = library.lookupFunction<
-        _CreateNative,
-        _Create>(
-      'terminal_create',
+    return isOpen;
+  }
+
+  bool write(
+    String text,
+  ) {
+    if (!isOpen) {
+      return false;
+    }
+
+    final ptr =
+        text.toNativeUtf8();
+
+    final ok =
+        TerminalFFI.instance.write(
+              _handle!,
+              ptr,
+              text.length,
+            ) !=
+            0;
+
+    malloc.free(ptr);
+
+    return ok;
+  }
+
+  String read([
+    int size = 4096,
+  ]) {
+    if (!isOpen) {
+      return '';
+    }
+
+    final buffer =
+        malloc<Utf8>(size);
+
+    final length =
+        TerminalFFI.instance.read(
+      _handle!,
+      buffer,
+      size,
     );
 
-    write = library.lookupFunction<
-        _WriteNative,
-        _Write>(
-      'terminal_write',
+    if (length <= 0) {
+      malloc.free(buffer);
+      return '';
+    }
+
+    final result =
+        buffer.cast<Utf8>().toDartString(
+              length: length,
+            );
+
+    malloc.free(buffer);
+
+    return result;
+  }
+
+  bool resize({
+    required int rows,
+    required int cols,
+  }) {
+    if (!isOpen) {
+      return false;
+    }
+
+    return TerminalFFI.instance.resize(
+          _handle!,
+          rows,
+          cols,
+        ) !=
+        0;
+  }
+
+  void close() {
+    if (!isOpen) {
+      return;
+    }
+
+    TerminalFFI.instance.close(
+      _handle!,
     );
 
-    read = library.lookupFunction<
-        _ReadNative,
-        _Read>(
-      'terminal_read',
-    );
-
-    resize = library.lookupFunction<
-        _ResizeNative,
-        _Resize>(
-      'terminal_resize',
-    );
-
-    close = library.lookupFunction<
-        _CloseNative,
-        _Close>(
-      'terminal_close',
-    );
+    _handle = nullptr;
   }
 }
