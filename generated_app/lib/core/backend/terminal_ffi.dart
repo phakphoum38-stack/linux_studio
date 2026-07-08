617d696465be5e6fa1ac02d75661c79bade05bd6
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-08 19:00:37 +0700

    Create terminal_ffi.dart

---
+++ generated_app/lib/core/backend/terminal_ffi.dart
@@ -0,0 +1,194 @@
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:ffi/ffi.dart';
+
+
+
+typedef TerminalCreateNative = Pointer<Void> Function(
+  Int32 rows,
+  Int32 cols,
+);
+
+
+typedef TerminalCreate =
+    Pointer<Void> Function(
+      int rows,
+      int cols,
+    );
+
+
+
+
+typedef TerminalWriteNative = Bool Function(
+  Pointer<Void> handle,
+  Pointer<Utf8> data,
+  Int32 length,
+);
+
+
+typedef TerminalWrite =
+    bool Function(
+      Pointer<Void> handle,
+      Pointer<Utf8> data,
+      int length,
+    );
+
+
+
+
+typedef TerminalReadNative = Int32 Function(
+  Pointer<Void> handle,
+  Pointer<Utf8> buffer,
+  Int32 size,
+);
+
+
+typedef TerminalRead =
+    int Function(
+      Pointer<Void> handle,
+      Pointer<Utf8> buffer,
+      int size,
+    );
+
+
+
+
+typedef TerminalResizeNative = Bool Function(
+  Pointer<Void> handle,
+  Int32 rows,
+  Int32 cols,
+);
+
+
+typedef TerminalResize =
+    bool Function(
+      Pointer<Void> handle,
+      int rows,
+      int cols,
+    );
+
+
+
+
+typedef TerminalCloseNative = Void Function(
+  Pointer<Void> handle,
+);
+
+
+typedef TerminalClose =
+    void Function(
+      Pointer<Void> handle,
+    );
+
+
+
+
+
+class TerminalFFI {
+
+
+  late DynamicLibrary library;
+
+
+
+  late TerminalCreate create;
+
+  late TerminalWrite write;
+
+  late TerminalRead read;
+
+  late TerminalResize resize;
+
+  late TerminalClose close;
+
+
+
+
+
+  TerminalFFI(){
+
+
+    if(
+      Platform.isWindows
+    ){
+
+      library =
+          DynamicLibrary.open(
+            'terminal_api.dll',
+          );
+
+
+    }
+
+
+    else
+
+    {
+
+      throw UnsupportedError(
+        'Windows only',
+      );
+
+    }
+
+
+
+
+
+    create =
+        library.lookupFunction<
+          TerminalCreateNative,
+          TerminalCreate
+        >(
+          'terminal_create',
+        );
+
+
+
+
+    write =
+        library.lookupFunction<
+          TerminalWriteNative,
+          TerminalWrite
+        >(
+          'terminal_write',
+        );
+
+
+
+
+    read =
+        library.lookupFunction<
+          TerminalReadNative,
+          TerminalRead
+        >(
+          'terminal_read',
+        );
+
+
+
+
+    resize =
+        library.lookupFunction<
+          TerminalResizeNative,
+          TerminalResize
+        >(
+          'terminal_resize',
+        );
+
+
+
+
+    close =
+        library.lookupFunction<
+          TerminalCloseNative,
+          TerminalClose
+        >(
+          'terminal_close',
+        );
+
+
+  }
+
+}
