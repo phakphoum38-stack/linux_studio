06156c8d6273c42ad728a9dacc196e6a97aa21e6
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-09 11:36:46 +0700

    Update terminal_screen.dart

--- generated_app/lib/screens/terminal_screen.dart
+++ generated_app/lib/screens/terminal_screen.dart
@@ -3,59 +3,137 @@ import 'package:xterm/xterm.dart';
 
 import '../core/controller/terminal_controller.dart' as app;
 
+
 class TerminalScreen extends StatefulWidget {
+
   const TerminalScreen({
     super.key,
   });
 
+
   @override
   State<TerminalScreen> createState() =>
       _TerminalScreenState();
+
 }
+
+
 
 class _TerminalScreenState
     extends State<TerminalScreen> {
-  late Terminal xterm;
 
-  late app.TerminalController controller;
+
+  late final Terminal xterm;
+
+
+  late final app.TerminalController controller;
+
+
 
   @override
-  void initState() {
+  void initState(){
+
     super.initState();
 
-    xterm = Terminal(
-      maxLines: 10000,
-    );
 
-    controller = app.TerminalController();
 
-    xterm.onOutput = (data) {
-      controller.write(data);
+    xterm =
+        Terminal(
+          maxLines: 10000,
+        );
+
+
+
+    controller =
+        app.TerminalController();
+
+
+
+    xterm.onOutput =
+        (data){
+
+      controller.write(
+        data,
+      );
+
     };
 
-    controller.start();
+
+
+    _start();
+
   }
+
+
+
+
+
+
+  Future<void> _start()
+  async {
+
+    await controller.start();
+
+  }
+
+
+
+
+
+
 
   @override
   Widget build(
     BuildContext context,
-  ) {
+  ){
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Linux Studio Terminal',
+
+      appBar:
+
+        AppBar(
+
+          title:
+
+            const Text(
+              'Linux Studio Terminal',
+            ),
+
         ),
-      ),
-      body: TerminalView(
-        xterm,
-        autofocus: true,
-      ),
+
+
+
+      body:
+
+        TerminalView(
+
+          xterm,
+
+          autofocus: true,
+
+        ),
+
     );
+
   }
 
+
+
+
+
+
+
   @override
-  void dispose() {
-    controller.stop();
+  void dispose(){
+
+
+    controller.dispose();
+
+
     super.dispose();
+
+
   }
+
+
 }
