d5642c3382c962328b0bf594f5f4329d9111d619
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-09 10:19:42 +0700

    Update terminal_screen.dart

--- generated_app/lib/screens/terminal_screen.dart
+++ generated_app/lib/screens/terminal_screen.dart
@@ -1,133 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:xterm/xterm.dart';
 
-import '../core/controller/terminal_controller.dart';
-
-
+import '../core/controller/terminal_controller.dart' as app;
 
 class TerminalScreen extends StatefulWidget {
-
   const TerminalScreen({
     super.key,
   });
 
-
   @override
-  State<TerminalScreen> createState()
-      => _TerminalScreenState();
-
+  State<TerminalScreen> createState() =>
+      _TerminalScreenState();
 }
-
-
-
 
 class _TerminalScreenState
     extends State<TerminalScreen> {
-
-
   late Terminal xterm;
 
-  late TerminalController controller;
-
-
+  late app.TerminalController controller;
 
   @override
-  void initState(){
-
+  void initState() {
     super.initState();
 
+    xterm = Terminal(
+      maxLines: 10000,
+    );
 
-    xterm =
-        Terminal(
-          maxLines: 10000,
-        );
+    controller = app.TerminalController();
 
-
-
-    controller =
-        TerminalController();
-
-
-
-    xterm.onOutput =
-        (data){
-
-          controller.write(
-            data,
-          );
-
-        };
-
-
+    xterm.onOutput = (data) {
+      controller.write(data);
+    };
 
     controller.start();
-
-
   }
-
-
-
-
-
-
 
   @override
   Widget build(
     BuildContext context,
-  ){
-
-
+  ) {
     return Scaffold(
-
-      appBar:
-
-        AppBar(
-
-          title:
-
-            const Text(
-              'Linux Studio Terminal',
-            ),
-
+      appBar: AppBar(
+        title: const Text(
+          'Linux Studio Terminal',
         ),
-
-
-
-      body:
-
-        TerminalView(
-
-          xterm,
-
-
-          autofocus: true,
-
-
-        ),
-
-
+      ),
+      body: TerminalView(
+        xterm,
+        autofocus: true,
+      ),
     );
-
-
   }
-
-
-
-
-
-
 
   @override
-  void dispose(){
-
-
+  void dispose() {
     controller.stop();
-
-
     super.dispose();
-
-
   }
-
-
 }
