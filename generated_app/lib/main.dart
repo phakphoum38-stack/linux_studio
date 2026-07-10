a6a76a40ea95b84e6b4ce2089e4d093ddaffd6bc
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-11 05:28:22 +0700

    Update main.dart

--- generated_app/lib/main.dart
+++ generated_app/lib/main.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/material.dart';
+
 import 'screens/terminal_screen.dart';
 
 void main() {
-  runApp(const LinuxStudioApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    const LinuxStudioApp(),
+  );
 }
 
 class LinuxStudioApp extends StatelessWidget {
-  const LinuxStudioApp({super.key});
+  const LinuxStudioApp({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Linux Studio Terminal v2',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: TerminalScreen(),
-      ),
+      theme: ThemeData.dark(),
+      home: const TerminalScreen(),
     );
   }
 }
