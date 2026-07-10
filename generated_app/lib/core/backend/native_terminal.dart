e1d87c87369f4b61c761d2d3fdbb66bf704191da
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-11 06:13:40 +0700

    Update native_terminal.dart

--- generated_app/lib/core/backend/native_terminal.dart
+++ generated_app/lib/core/backend/native_terminal.dart
@@ -5,115 +5,244 @@ import 'package:ffi/ffi.dart';
 
 import 'terminal_ffi.dart';
 
+
 class NativeTerminal {
+
+
   NativeTerminal({
     this.rows = 24,
     this.cols = 80,
   });
 
+
+
   final int rows;
   final int cols;
 
+
+
   Pointer<Void>? _handle;
+
+
 
   bool get isOpen =>
       _handle != null &&
       _handle != nullptr;
 
-  bool open() {
-    if (isOpen) {
+
+
+
+
+  bool open(){
+
+    if(isOpen){
       return true;
     }
 
-    _handle = TerminalFFI.instance.create(
-      rows,
-      cols,
-    );
 
-    return isOpen;
+
+    try {
+
+      TerminalFFI.instance.load();
+
+
+
+      _handle =
+          TerminalFFI.instance.create(
+            rows,
+            cols,
+          );
+
+
+
+      return isOpen;
+
+
+    }
+
+    catch(e){
+
+      return false;
+
+    }
+
   }
+
+
+
+
+
+
+
+
 
   bool write(
     String text,
-  ) {
-    if (!isOpen) {
+  ){
+
+    if(!isOpen){
       return false;
     }
 
-    final ptr =
+
+
+    final data =
         text.toNativeUtf8();
 
+
+
     try {
-      return TerminalFFI.instance.write(
+
+      final result =
+          TerminalFFI.instance.write(
             _handle!,
-            ptr,
+            data,
             utf8.encode(text).length,
-          ) !=
-          0;
-    } finally {
-      malloc.free(ptr);
+          );
+
+
+
+      return result != 0;
+
+
     }
+
+    finally {
+
+      malloc.free(data);
+
+    }
+
   }
 
+
+
+
+
+
+
+
+
   String read([
-    int bufferSize = 8192,
-  ]) {
-    if (!isOpen) {
+    int size = 8192,
+  ]){
+
+
+    if(!isOpen){
       return '';
     }
 
+
+
     final buffer =
         calloc<Uint8>(
-      bufferSize,
-    );
+          size,
+        );
+
+
 
     try {
+
+
       final length =
           TerminalFFI.instance.read(
-        _handle!,
-        buffer,
-        bufferSize,
-      );
+            _handle!,
+            buffer,
+            size,
+          );
 
-      if (length <= 0) {
+
+
+      if(length <= 0){
         return '';
       }
+
+
+
 
       return utf8.decode(
         buffer.asTypedList(
           length,
         ),
+        allowMalformed:true,
       );
-    } finally {
-      calloc.free(buffer);
+
+
+
     }
+
+    finally {
+
+      calloc.free(
+        buffer,
+      );
+
+    }
+
+
   }
 
+
+
+
+
+
+
+
+
   bool resize({
+
     required int cols,
+
     required int rows,
-  }) {
-    if (!isOpen) {
+
+  }){
+
+
+    if(!isOpen){
       return false;
     }
 
-    return TerminalFFI.instance.resize(
+
+
+    return
+        TerminalFFI.instance.resize(
           _handle!,
           rows,
           cols,
-        ) !=
-        0;
+        ) != 0;
+
+
   }
 
-  void close() {
-    if (!isOpen) {
+
+
+
+
+
+
+
+
+  void close(){
+
+
+    if(!isOpen){
       return;
     }
+
+
 
     TerminalFFI.instance.close(
       _handle!,
     );
 
+
+
     _handle = nullptr;
+
+
   }
+
+
+
 }
