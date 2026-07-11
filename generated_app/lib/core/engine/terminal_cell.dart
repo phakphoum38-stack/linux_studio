5e090af5952920966439a8b11d3a2b5f88fb75af
phakphoum38-stack <phakphoum38@gmail.com>
2026-07-11 21:55:32 +0700

    Update terminal_cell.dart

--- generated_app/lib/core/engine/terminal_cell.dart
+++ generated_app/lib/core/engine/terminal_cell.dart
@@ -1,116 +1,43 @@
 class TerminalCell {
-
-
   String char;
-
 
   int foreground;
 
-
   int background;
-
 
   bool bold;
 
-
   bool underline;
-
 
   bool italic;
 
-
-  bool inverse;
-
-
-
-
-
   TerminalCell({
-
     this.char = ' ',
-
     this.foreground = 37,
-
     this.background = 40,
-
     this.bold = false,
-
     this.underline = false,
-
     this.italic = false,
-
-    this.inverse = false,
-
   });
 
+  int get fg => foreground;
 
+  set fg(int value) {
+    foreground = value;
+  }
 
+  int get bg => background;
 
+  set bg(int value) {
+    background = value;
+  }
 
-
-
-
-  void clear()
-
-  {
-
-
+  void clear() {
     char = ' ';
-
-
     foreground = 37;
-
-
     background = 40;
-
-
-
     bold = false;
-
-
     underline = false;
-
-
     italic = false;
-
-
-    inverse = false;
-
-
   }
-
-
-
-
-
-
-
-  TerminalCell copy()
-
-  {
-
-
-    return TerminalCell(
-
-      char: char,
-
-      foreground: foreground,
-
-      background: background,
-
-      bold: bold,
-
-      underline: underline,
-
-      italic: italic,
-
-      inverse: inverse,
-
-    );
-
-
-  }
-
-
-
 }
